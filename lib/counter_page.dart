7b2362cf361bf627bfed4b9db658a54c56a0f245
AA919Rabby <roycerabbikhan999@gmail.com>
2025-11-07 03:25:39 -1100

    Initial Flutter app upload

---
+++ lib/counter_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int counter=0;
+  incrementCounter(){
+    setState(() {
+      counter++;
+    });
+  }
+  decrementCounter(){
+    setState(() {
+      counter--;
+    });
+  }
+    resetCounter(){
+    setState(() {
+      counter=0;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: Center(child: Text('Counter app',style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold,color: Colors.white),)),
+        backgroundColor: Colors.blueGrey,
+      ),
+      backgroundColor: Colors.black,
+      body: Center(
+          child: Column(
+            children: [
+              Text(
+                counter.toString(),
+                style: TextStyle(color: Colors.white,fontSize: 200),
+              ),
+              SizedBox(height: 50,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                      onPressed: (){
+                        incrementCounter();
+                      },
+                      child: Icon(Icons.add)),
+                  ElevatedButton(
+                      onPressed: (){
+                        resetCounter();
+                      },
+                      child: Icon(Icons.restore)),
+                  ElevatedButton(
+                      onPressed: (){
+                        decrementCounter();
+                      },
+                      child: Icon(Icons.remove)),
+                ],
+              ),
+            ],
+          ),
+        ),
+    );
+  }
+}
